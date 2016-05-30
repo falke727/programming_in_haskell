6def9cfc69d9c52689806b1635cc712f8e35d36f
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-05-31 08:34:47 +0900

    add 8_7.hs

---
+++ 08/8_7.hs
@@ -0,0 +1,34 @@
+import Parser hiding (expr, term, factor, eval)
+
+-- expr   ::= term ('+' expr | '-' expr | e)
+-- term   ::= base ('*' term | '/' expr | e)
+-- base   ::= factor ('^' term | ε)
+-- factor ::= '(' expr ')' | nat
+-- nat    ::= '0' | '1' | '2' | ...
+
+ufloat :: Parser Float
+ufloat = many1 digit >>= \xs -> ((symbol ".") >>= \_ -> many1 digit >>= \ys -> return $ read (xs ++ "." ++ ys)) +++ (return $ read xs)
+
+float :: Parser Float
+float = ((char '-') >>= \_ -> ufloat >>= \f -> return (-f)) +++ ufloat
+
+floating :: Parser Float
+floating = token float
+
+expr :: Parser Float
+expr = term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ (symbol "-" >>= \_ -> expr >>= \e -> return (t-e)) +++ return t
+
+term :: Parser Float
+term = base >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b
+
+base :: Parser Float
+base = factor >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f
+
+factor :: Parser Float
+factor = (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) +++ floating
+
+eval :: String -> Float
+eval xs = case parse expr xs of
+  [(f,[])] -> f
+  [(_,out)] -> error ("unused input " ++ out)
+  [] -> error "invalid input"
