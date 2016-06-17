569faeefdf2cd377f5e0a6ca7429d7c1bd4e5332
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-06-18 07:06:11 +0900

    add 8_8.hs

--- 08/8_8.hs
+++ 08/8_8.hs
@@ -1,4 +1,4 @@
-import Parser hiding (expr, term, factor, eval)
+import Parser hiding (expr, term, factor, eval, int)
 
 -- expr   ::= term '+' expr | expr '-' term | term
 -- term   ::= base ('*' term | e) | expr '/' base
@@ -6,29 +6,47 @@ import Parser hiding (expr, term, factor, eval)
 -- factor ::= '(' expr ')' | nat
 -- nat    ::= '0' | '1' | '2' | ...
 
-ufloat :: Parser Float
-ufloat = many1 digit >>= \xs -> ((symbol ".") >>= \_ -> many1 digit >>= \ys -> return $ read (xs ++ "." ++ ys)) +++ (return $ read xs)
+{- 上記の素朴な定義を実装すると左再帰の無限ループに陥って一巻の終わり．
+     expr => expr '-' term => (expr '-' term) '-' term => ((expr '-' term) '-' term) '-' term => ...
+   よって，
 
-float :: Parser Float
-float = ((char '-') >>= \_ -> ufloat >>= \f -> return (-f)) +++ ufloat
+     foldl :: (a -> b -> a) -> a -> [b] -> a
+     foldl f v []     = v
+     foldl f v (x:xs) = foldl f (f v x) xs
 
-floating :: Parser Float
-floating = token float
+  と
 
-expr :: Parser Float
-expr = (term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e))) +++ (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t)) +++ term
+     many :: Parser a -> Parser [a]
+     many p = many1 p +++ return []
 
-term :: Parser Float
-term = base >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b
+     _>>=_ :: Parser a -> (a -> Parser b) -> Parser b
+     p >>= f  = P (\inp -> case parse p inp of
+                    []        -> []
+                    [(v,out)] -> parse (f v) out)
 
-base :: Parser Float
-base = factor >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f
+     many1 :: Parser a -> Parser [a]
+     many1 p = p >>= \v -> many p >>= \vs -> return (v:vs)
 
-factor :: Parser Float
-factor = (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) +++ floating
+  を用いて expr を実装し直す．-}
 
-eval :: String -> Float
+int :: Parser Int
+int = (token (char '-') >>= \_ -> natural >>= \n -> return (-n)) +++ natural
+
+-- https://gist.github.com/yamamotoj/f8ddb5cead527344268a を natural の部分以外全部（本質部分）を真似しました．
+expr :: Parser Int
+--expr = term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t
+expr = term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e))
+                      +++ (many (symbol "-" >>= \_ -> term >>= \n -> return n) >>= \ss -> return (foldl (-) t ss))
+                      +++ return t
+
+term :: Parser Int
+term = factor >>= \f -> (symbol "*" >>= \_ -> term >>= \t -> return (f*t)) +++ return f
+
+factor :: Parser Int
+factor = (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) +++ int
+
+eval :: String -> Int
 eval xs = case parse expr xs of
-  [(f,[])] -> f
+  [(n,[])]  -> n
   [(_,out)] -> error ("unused input " ++ out)
-  [] -> error "invalid input"
+  []        -> error "invalid input"
