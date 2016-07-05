f0783e301fd49f609c95a543efee0286dcaf13a6
falke727 <r201470039hs@kanagawa-u.ac.jp>
2016-07-05 09:12:22 +0900

    add 10 directory

---
+++ 09/8_8.hs
@@ -0,0 +1,111 @@
+import Parser hiding (expr, term, factor, eval, int)
+
+-- expr   ::= term '+' expr | expr '-' term | term
+-- term   ::= base ('*' term | e) | expr '/' base
+-- base   ::= factor ('^' term | ε)
+-- factor ::= '(' expr ')' | nat
+-- nat    ::= '0' | '1' | '2' | ...
+
+{- 上記の素朴な定義を実装すると左再帰の無限ループに陥って一巻の終わり．
+     expr => expr '-' term => (expr '-' term) '-' term => ((expr '-' term) '-' term) '-' term => ...
+   よって，
+
+     foldl :: (a -> b -> a) -> a -> [b] -> a
+     foldl f v []     = v
+     foldl f v (x:xs) = foldl f (f v x) xs
+
+  と
+
+     many :: Parser a -> Parser [a]
+     many p = many1 p +++ return []
+
+     _>>=_ :: Parser a -> (a -> Parser b) -> Parser b
+     p >>= f  = P (\inp -> case parse p inp of
+                    []        -> []
+                    [(v,out)] -> parse (f v) out)
+
+     many1 :: Parser a -> Parser [a]
+     many1 p = p >>= \v -> many p >>= \vs -> return (v:vs)
+
+  を用いて expr を実装し直す．-}
+
+int :: Parser Int
+int = (token (char '-') >>= \_ -> natural >>= \n -> return (-n)) +++ natural
+
+{-
+https://gist.github.com/yamamotoj/f8ddb5cead527344268a を natural の部分以外全部（本質部分）を真似したのが以下．
+expr :: Parser Int
+expr = term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e))
+                      +++ (many (symbol "-" >>= \_ -> term >>= \n -> return n) >>= \ss -> return (foldl (-) t ss))
+                      +++ return t
+しかし，これだと
+  $eval "3-3+3+4" == *** Exception: unused input +3+4
+となってしまう．
+-}
+
+expr :: Parser Int
+expr = term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e))
+                      +++ (many (symbol "-" >>= \_ -> term >>= \n -> return n) >>= \ss -> let sub = foldl (-) t ss in (symbol "+" >>= \_ -> expr >>= \e -> return (sub + e)) +++ return sub)
+                      +++ return t
+
+term :: Parser Int
+term = factor >>= \f -> (symbol "*" >>= \_ -> term >>= \t -> return (f*t))
+--                        +++ (many (symbol "/" >>= \_ -> factor >>= \n -> return n) >>= \ss -> return (foldl (div) f ss))
+                        +++ (many (symbol "/" >>= \_ -> factor >>= \n -> return n) >>= \ss -> let dd = foldl (div) f ss in (symbol "*" >>= \_ -> term >>= \t -> return (dd * t)) +++ return dd)
+                        +++ return f
+
+factor :: Parser Int
+factor = (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) +++ int
+
+eval :: String -> Int
+eval xs = case parse expr xs of
+  [(n,[])]  -> n
+  [(_,out)] -> error ("unused input " ++ out)
+  []        -> error "invalid input"
+
+{-
+parse expr "3-4"
+parse ((term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) +++ (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t))) "3-4"
+parse ((term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) +++ (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t))) "3-4"
+parse ((term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) `mplut` (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t))) "3-4"
+parse (P (\inp -> case parse (term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) inp of 
+           [] -> parse (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t)) inp
+           [(v,out)] -> [(v,out)]))
+      "3-4"
+(\inp -> case parse (term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) inp of
+  [] -> parse (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t)) inp
+  [(v,out)] -> [(v,out)])
+  "3-4"
+case parse (term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4" of
+ [] -> parse (expr >>= \e -> symbol "-" >>= \_ -> term >>= \t -> return (e-t)) "3-4"
+ [(v,out)] -> [(v,out)]
+
+    ------------------------------------------------------------------------------------------------------
+      parse (term >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      parse ((base >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+               >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      parse (((factor >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f)
+                >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+                 >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      parse (((((symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) +++ floating)
+                  >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f)
+                   >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+                    >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      parse (((((symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) `mplus` floating)
+                  >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f)
+                   >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+                    >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      parse (((((P (inp -> case parse (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) inp of
+                     [] -> parse floating inp 
+                     [(v,out)] -> [(v,out)]))
+                  >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f)
+                   >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+                    >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t) "3-4"
+      (((case parse (symbol "(" >>= \_ -> expr >>= \e -> symbol ")" >>= \_ -> return e) "3-4" of
+                     [] -> parse floating "3-4"
+                     [(v,out)] -> [(v,out)])
+                  >>= \f -> (symbol "^" >>= \_ -> term >>= \t -> return (f**t)) +++ return f)
+                   >>= \b -> (symbol "*" >>= \_ -> term >>= \t -> return (b*t)) +++ (symbol "/" >>= \_ -> term >>= \t -> return (b/t)) +++ return b)
+                    >>= \t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t+e)) +++ return t
+    ------------------------------------------------------------------------------------------------------
+-}
---
+++ 10/10_body.hs
@@ -0,0 +1,188 @@
+import Prelude hiding (Right, Left)
+
+type String' = [Char]
+type Board = [Pos]
+type Pos = (Int,Int)
+
+-- error -- type Tree = (Int,[Tree])
+
+type Parser a = String -> [(a,String)]
+data World = World -- The Data type "World" does not exist.
+type IO a = World -> (a, World)
+
+type Assoc k v = [(k,v)]
+
+hoge :: Assoc Int String
+hoge = [(1,"kuwahara"), (2,"elian"), (3,"miyazaki"), (4,"tsutsugoh")]
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [ v | (k', v) <- t, k == k' ]
+
+data Bool' = True' | False'
+
+data Move = Left | Right | Up | Down
+
+move :: Move -> Pos -> Pos
+move Left  (x,y) = (x-1,y)
+move Right (x,y) = (x+1,y)
+move Up    (x,y) = (x,y+1)
+move Down  (x,y) = (x,y-1)
+
+moves :: [Move] -> Pos -> Pos
+moves [] p = p
+moves (m:ms) p = moves ms (move m p)
+
+moveExample = [Right,Up,Right,Up,Right,Down,Left]
+-- moves moveExample (0,0)
+
+data Shape = Circle Float | Rect Float Float
+
+square :: Float -> Shape
+square n = Rect n n
+
+area :: Shape -> Float
+area (Circle r) = pi * r ^ 2
+area (Rect x y) = x * y
+
+data Maybe' a = Just' a | Nothing'
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv m n = Just (m `div` n)
+
+safehead :: [a] -> Maybe a
+safehead []     = Nothing
+safehead (x:xs) = Just x
+
+data Nat = Zero | Suc Nat deriving (Show)
+
+nat2int :: Nat -> Int
+nat2int Zero    = 0
+nat2int (Suc x) = 1 + (nat2int x)
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Suc (int2nat (n-1))
+
+add :: Nat -> Nat -> Nat
+add m n = int2nat (nat2int m + nat2int n)
+
+add' :: Nat -> Nat -> Nat
+add' Zero    n = n
+add' (Suc m) n = Suc (add' m n)
+
+data List a = Nil | Cons a (List a) deriving (Show)
+
+len :: List a -> Int
+len Nil         = 0
+len (Cons _ xs) = 1 + len xs
+
+tarao :: List Int
+tarao = Cons 4 (Cons 3 (Cons 2 (Cons 1 (Cons 0 Nil))))
+
+data Tree = Leaf Int | Node Tree Int Tree deriving (Show)
+
+t :: Tree
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Int -> Tree -> Bool
+occurs m (Leaf n)     = m == n
+occurs m (Node l n r) = (occurs m l) || m == n || (occurs m r)
+
+flatten :: Tree -> [Int]
+flatten (Leaf n) = [n]
+flatten (Node l n r) = flatten l ++ [n] ++ flatten r
+
+-- The Tree defined in this program is sorted.
+occurs' :: Int -> Tree -> Bool
+occurs' m (Leaf n) = m == n
+occurs' m (Node l n r)
+  | m == n    = True
+  | m <  n    = occurs' m l
+  | otherwise = occurs' m r
+
+data TreeA a = LeafA a | NodeA (TreeA a) (TreeA a)
+data TreeB a = LeafB | NodeB (TreeB a) a (TreeB a)
+data TreeC a b = LeafC a | NodeC (TreeC a b) b (TreeC a b)
+data TreeD a = NodeD a [TreeD a]
+
+tD :: TreeD Int
+tD = NodeD (-2) [NodeD (-1) [NodeD 0 [NodeD 1 [], NodeD 2 [], NodeD 3 []]]]
+
+data Prop =
+    Const Bool
+  | Var   Char
+  | Not   Prop
+  | And   Prop Prop
+  | Imply Prop Prop
+  deriving (Show)
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A')) -- A ∧ ¬A
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A') -- (A ∧ B) ⊃ A
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B')) -- A ⊃ (A ∧ B)
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B') -- (A ∧ (A ⊃ B)) ⊃ B
+
+top :: Prop
+top = Const True
+
+bot :: Prop
+bot = Const False
+
+type Subst = Assoc Char Bool -- Assign（付値）
+
+katsuo :: Subst
+katsuo = [('A', False), ('B', True)]
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b)   = b
+eval s (Var x)     = find x s
+eval s (Not p)     = not (eval s p)
+eval s (And p q)   = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+type Bit = Int
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+bools' :: Int -> [[Bool]]
+bools' n = map (map conv . make n . int2bin) [0..limit]
+  where
+    limit = (2^n) - 1
+    make n bs = take n (bs ++ repeat 0)
+    conv 0 = False
+    conv 1 = True
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+  where bss = bools (n-1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+  where vs = rmdups (vars p)
+
+isTautology :: Prop -> Bool
+isTautology p = and [eval s p | s <- substs p]
+
+isSatisfiable :: Prop -> Bool
+isSatisfiable p = or [eval s p | s <- substs p]
